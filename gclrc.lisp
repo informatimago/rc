8c3b3edcc95427bf46ecc2d498cff85a0220a0e8
Pascal J. Bourguignon <pjb@informatimago.com>
2013-07-02 09:13:14 +0200

    Patched make-pathname.

--- gclrc.lisp
+++ gclrc.lisp
@@ -30,25 +30,76 @@
 ;;;;    PURPOSE.  See the GNU General Public License for more details.
 ;;;;    
 ;;;;    You should have received a copy of the GNU General Public
+
+
+
+;;----------------------------------------------------------------------
+;; Setting environment -- gcl part --
+;; ----------------------------------
+(in-package "USER")
+(RENAME-PACKAGE "LISP" "LISP" (list*  "CL" "COMMON-LISP"
+                                      (copy-seq (PACKAGE-NICKNAMES "LISP"))))
+(RENAME-PACKAGE "USER" "USER" (list*  "CL-USER" "COMMON-LISP-USER"
+                                      (copy-seq (PACKAGE-NICKNAMES "USER"))))
+(in-package "COMMON-LISP-USER")
+ 
+#||
+(defun post-process-logical-pathname-translations (host)
+  (flet ((pstring (x) (if (pathnamep x) (namestring x) (string x))))
+    (setf (logical-pathname-translations host)
+          (sort (remove-if
+                 (lambda (p) (let ((p (pstring p)))
+                          (and (< 5 (length p))
+                               (string= "*.*.*" p :start2 (- (length p) 5)))))
+                 (logical-pathname-translations host)
+                 :key (function first))
+                (lambda (a b) (> (length (pstring (first a)))
+                            (length (pstring (first b)))))))))
+||#
+
+
 ;;----------------------------------------------------------------------
 ;; Setting environment -- COMMON-LISP part --
 ;; ------------------------------------------
 
 
+(defun mixed-case-p (string)
+  "
+RETURN:         Whether the STRING contains both upper case characters
+                and lower  case characters.   If there  are characters
+                that are both-case-p, then mixed-case-p returns true.
+"
+  (and (some (function upper-case-p) string)
+       (some (function lower-case-p) string)))
 
 
 (defvar *original-make-pathname* (function make-pathname))
-(defun make-pathname (&rest arguments &key (case :local) &allow-other-keys)
+(defun make-pathname (&rest arguments &key directory (case :local) &allow-other-keys)
+  (when directory
+       (ecase (first directory)
+         ((:relative) (pop directory))
+         ((:absolute) (pop directory) (push "" directory))))
   (ecase case
-    ((:local)  (apply *original-make-pathname* :allow-other-keys t arguments))
-    ((:common) (apply *original-make-pathname* :allow-other-keys t
-                      (mapcar (lambda (argument)
-                                (if (string))
-                                )
-                              arguments))))
-  )
+    ((:local)
+     (apply *original-make-pathname* :allow-other-keys t arguments))
+    ((:common)
+     (let ((arguments (mapcar (lambda (argument)
+                                (flet ((convert (argument)
+                                         (if (stringp argument)
+                                             (if (mixed-case-p argument)
+                                                 (error "~S is not in :COMMON case." argument)
+                                                 (string-downcase argument))
+                                             argument)))
+                                  (typecase argument
+                                    (string  (convert argument))
+                                    (list    (mapcar (function convert) argument))
+                                    (t       argument))))
+                              (if directory
+                                  (list* :directory directory arguments)
+                                  arguments))))
+       (apply *original-make-pathname* :allow-other-keys t arguments)))))
 
-(SETF *LOAD-VERBOSE* NIL)
+(SETF *LOAD-VERBOSE* t)
 (LOAD (MERGE-PATHNAMES
        (MAKE-PATHNAME :DIRECTORY '(:RELATIVE "RC") :NAME "COMMON" :TYPE "LISP"
                       :CASE :COMMON)
