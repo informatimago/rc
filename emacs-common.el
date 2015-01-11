556441b0ad874a1c8e4a7832f9d3fe9ae8b50a6a
Pascal J. Bourguignon <pjb@informatimago.com>
2015-01-11 01:06:15 +0100

    Extracted pjb-speak.el pjb-erc-speak.el

--- emacs-common.el
+++ emacs-common.el
@@ -67,7 +67,6 @@ please, use `add-lac' and `remove-lac' instead of accessing this list directly."
     (ignore-errors (funcall lac))))
 
 (defvar shell-file-name "/bin/bash")
-
 (defvar *tempdir*  (format "/tmp/emacs%d" (user-uid)))
 
 (defvar *hostname*
@@ -2518,6 +2517,9 @@ URL in a new window."
 
 ;;;----------------------------------------------------------------------------
 
+;;;----------------------------------------------------------------------------
+(require 'pjb-speak)
+(require 'pjb-erc-speak)
 
 ;;;----------------------------------------------------------------------------
 
@@ -2661,197 +2663,6 @@ License:
 
 ;; (add-hook 'erc-join-hook (lambda () (local-set-key (kbd "H-a") 'pjb-erc-answer)))
 (global-set-key (kbd "H-a") 'pjb-erc-answer)
-
-;;;----------------------------------------------------------------------------
-
-(defvar *pjb-speak-file-counter* 0)
-
-(defun pjb-speak-file ()
-  (format "%s/speak-%d.txt" *tempdir* (incf *pjb-speak-file-counter*)))
-
-
-(defvar *pjb-speak-last-message* nil)
-
-(defun* speak (message &key voice language)
-  (interactive "sMessage: ")
-  (let ((file (pjb-speak-file)))
-    (with-current-buffer (get-buffer-create " *speak text*")
-      (erase-buffer)
-      (insert message)
-      (setf *pjb-speak-last-message* message)
-      (write-region (point-min) (point-max) file))
-    (let ((command  (format "speak %s %s -f %s"
-                            (if voice    (format "-v %s" voice)    "")
-                            (if language (format "-l %s" language) "")
-                            file)))
-      (message "%S" command)
-      (shell-command command))))
-
-(defalias 'say 'speak)
-
-(defun speak-repeat ()
-  (interactive)
-  (speak *pjb-speak-last-message*))
-
-
-
-(defparameter *pjb-erc-spoken-nicks*
-  '(("\\<e1f\\>"          . "elf")
-    ("\\<tali[0-9]+"      . "tali")
-    ("\\<fsbot\\>"        . "F. S. Bot")
-    ("\\<qu1j0t3\\>"      . "quijote")
-    ("\\<chromaticwt\\>"  . "chromatic W. T.")
-    ("\\<jcowan\\>"       . "J. Cowan")
-    ("\\<cky\\>"          . "C. K. Y.")
-    ("\\<pjb\\>"          . "Pascal")
-    ("\\<H4ns\\>"         . "Hans")
-    ("\\<Corman[0-9]+\\>" . "Corman"))
-  "An a-list mapping regexps of nicks to the corresponding text to be read aloud.")
-
-
-(defun pjb-erc-spoken-nick (nick)
-  "
-RETURN:  The text to be read aloud for the `nick' in `*pjb-erc-spoken-nicks*'.
-"
-  (let ((entry (assoc* nick *pjb-erc-spoken-nicks*
-                       :test (lambda (nick ref) (string-match ref nick)))))
-    (if entry
-        (cdr entry)
-        nick)))
-
-
-(defun erc-response.recipient (response)
-  (first (erc-response.command-args response)))
-
-(defun erc-response.sender-nick (response)
-  (let ((sender (erc-response.sender response)))
-   (subseq sender 0 (position ?! sender))))
-
-
-(defparameter *pjb-erc-massage-substitutions*
-  '(("\\<pjb\\>"                 "Pascal")
-    ("\\<CL\\>"                  "See Ell") 
-    ("\\<C-"                     "Control-")
-    ("\\<M-"                     "Meta-")
-    ("\\<A-"                     "Alt-")
-    ("\\<S-"                     "Shift-")
-    ("\\<s-"                     "super-")
-    ("\\<H-"                     "Hyper-")
-    ("\\(:-?)\\|(-?:\\)"         "AhAhAh!")
-    (":-?("                      "BooBooBoo!")
-    (":-/"                       "muek")
-    (":-?[Pp]"                   "bruu")
-    ("\\<\\(ty\\|thx\\)\\>"      "Thank you!")
-    ("\\<LOL\\>"                 "AhAhAh! Laughting Out Loud!") 
-    ("\\<ROFL\\>"                "AhAhAh! Rolling On the Floor!")
-    ("\\<hrm\\>"                 "errrmmm") 
-    ("\\<btw\\>"                 "by the way")
-    ("\\<wtf\\>"                 "what the fuck")
-    ("\\<imo\\>"                 "in my opinion")
-    ("\\<imho\\>"                "in my humble opinion")
-    ("\\<imnsho\\>"              "in my not so humble opinion")))
-
-
-(defun pjb-erc-massage-message (message)
-  (with-current-buffer (get-buffer-create "*pjb massage text*")
-    (erase-buffer)
-    (insert message)
-    (let ((case-fold-search nil))
-      (loop
-         for (reg sub) in *pjb-erc-massage-substitutions*
-         do (progn
-              (goto-char (point-min))
-              (loop
-                 while (re-search-forward reg nil t)
-                 do (progn
-                      (delete-region (match-beginning 0) (match-end 0))
-                      (insert sub))))))
-    (buffer-string)))
-
-
-
-(defvar *pjb-erc-speak-reject-recipient* '()
-  "can be:
-nil   don't reject any channel.
-:all  reject every channel.
-or a list of nicknames or channel names \"nick\" \"\#chan\"
-to reject (never speak them aloud).
-See: `*pjb-erc-speak-reject-sender*', `*pjb-erc-speak-accept-sender*',
-      and `pjb-erc-privmsg-meat'.
-
-Messages are spoken if the recipient
-")
-
-(defvar *pjb-erc-speak-reject-sender* '()
-  "can be:
-nil   don't reject anybody.
-:all  reject everybody.
-or a list of nicknames or channel names \"nick\" \"\#chan\"
-to reject (never speak them aloud).
-See: `*pjb-erc-speak-reject-recipient*', `*pjb-erc-speak-accept-sender*',
-      and `pjb-erc-privmsg-meat'.
-")
-
-(defvar *pjb-erc-speak-accept-sender* '()
-  "can be:
-nil   don't accept anything.
-:all  accept everything.
-or a list of nicknames or channel names \"nick\" \"\#chan\"
-to accept (speak them aloud).
-See: `*pjb-erc-speak-reject-recipient*', `*pjb-erc-speak-reject-sender*',
-      and `pjb-erc-privmsg-meat'.
-")
-
-(setf *pjb-erc-speak-reject-recipient* '("#emacs")
-      *pjb-erc-speak-reject-recipient* :all
-      *pjb-erc-speak-reject-sender*    :all
-      *pjb-erc-speak-accept-sender*    '("Posterdati" "pjb-"))
-
-
-(defvar *pjb-erc-speak-last-speaker* nil)
-
-
-(defun pjb-erc-privmsg-meat (process response)
-  "The messages are spoken if the sender is in `*pjb-erc-speak-accept-sender*',
-or the sender is not in `*pjb-erc-speak-reject-sender*',
-or the recipient is not in `*pjb-erc-speak-reject-recipient*',
-"
-  (when (or
-         (case *pjb-erc-speak-accept-sender*
-           ((nil)    nil)
-           ((:all t) t)
-           (otherwise (member* (erc-response.sender-nick response)
-                               *pjb-erc-speak-accept-sender* :test 'string=)))
-         (case *pjb-erc-speak-reject-sender*
-           ((nil)    t)
-           ((:all t) nil)
-           (otherwise (not (member* (erc-response.sender-nick response)
-                                    *pjb-erc-speak-reject-sender* :test 'string=))))
-         (case *pjb-erc-speak-reject-recipient*
-           ((nil)    t)
-           ((:all t) nil)
-           (otherwise (not (member* (erc-response.recipient response)
-                                    *pjb-erc-speak-reject-recipient* :test 'string=)))))
-    (speak (let* ((nick (pjb-erc-spoken-nick (erc-response.sender-nick response)))
-                  (chan (pjb-erc-spoken-nick (remove ?# (erc-response.recipient response))))
-                  (mesg (pjb-erc-massage-message (erc-response.contents response))))
-             (if (equal *pjb-erc-speak-last-speaker*
-                        (cons nick chan))
-                 (format "%s" mesg)
-                 (progn
-                   (setf *pjb-erc-speak-last-speaker* (cons nick chan))
-                   (format "%s said to %s: ... %s" nick chan mesg))))))
-  nil)
-
-
-(defun pjb-erc-speak-on ()
-  (interactive)
-  (pushnew 'pjb-erc-privmsg-meat  erc-server-PRIVMSG-functions))
-
-(defun pjb-erc-speak-off  ()
-  (interactive)
-  (setf erc-server-PRIVMSG-functions
-        (remove 'pjb-erc-privmsg-meat  erc-server-PRIVMSG-functions)))
 
 ;;;----------------------------------------------------------------------------
 (.EMACS "server")
