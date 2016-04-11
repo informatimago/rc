959e9e87203a27f2c2d3f9475cf71e0197a3f6c3
Pascal J. Bourguignon <pjb@informatimago.com>
2016-04-11 11:09:13 +0200

    Changed at trustonic.

--- emacs-ruby.el
+++ emacs-ruby.el
@@ -12,8 +12,8 @@
 
 (require 'smartparens-config)
 (require 'smartparens-ruby)
-(smartparens-global-mode)
-(show-smartparens-global-mode t)
+;; (smartparens-global-mode)
+;; (show-smartparens-global-mode t)
 ;; (sp-with-modes '(rhtml-mode)
 ;;                (sp-local-pair "<" ">")
 ;;                (sp-local-pair ""))
