dea6bf52ab2d3fb9f234ff3c23864a4dbab92eea
Pascal J. Bourguignon <pjb@informatimago.com>
2020-10-21 13:30:10 +0200

    Updates to the haskell working environment. Corrected package initialization.

--- emacs-package.el
+++ emacs-package.el
@@ -8,92 +8,92 @@
 (require 'gnutls)
 (add-to-list 'gnutls-trustfiles "/etc/ssl/cert.pem")
 
-(when (and (zerop (length (shell-command-to-string "git config --global http.proxy")))
-           (require 'package nil t))
-  ;; Anyn add to list for package-archives (to add marmalade or melpa) goes here
-  (setq package-archives '(("gnu"           . "http://elpa.gnu.org/packages/")
-                           ("melpa"         . "https://melpa.org/packages/")
-                           ;; ("marmalade"     . "https://marmalade-repo.org/packages/")
-                           ;; ("melpa-stable"  . "https://stable.melpa.org/packages/")
-                           ;; ("org"           . "https://orgmode.org/elpa/")
-                           ))
+(.EMACS "Initializing emacs packages.")
+;; Anyn add to list for package-archives (to add marmalade or melpa) goes here
+(setq package-archives '(("gnu"           . "http://elpa.gnu.org/packages/")
+                         ("melpa"         . "https://melpa.org/packages/")
+                         ;; ("marmalade"     . "https://marmalade-repo.org/packages/")
+                         ;; ("melpa-stable"  . "https://stable.melpa.org/packages/")
+                         ;; ("org"           . "https://orgmode.org/elpa/")
+                         ))
 
-  (package-initialize)
-  (package-refresh-contents)
-
-
-  ;; (defun package-update-load-path ()
-  ;;   "Update the load path for newly installed packages."
-  ;;   (interactive)
-  ;;   (let ((package-dir (expand-file-name package-user-dir)))
-  ;;     (mapc (lambda (pkg)
-  ;;             (let ((stem (symbol-name (car pkg)))
-  ;;                   (version "")
-  ;;                   (first t)
-  ;;                   path)
-  ;;               (mapc (lambda (num)
-  ;;                       (if first
-  ;;                         (setq first nil)
-  ;;                         (setq version (format "%s." version)))
-  ;;                       (setq version (format "%s%s" version num)))
-  ;;                     (aref (cdr pkg) 0))
-  ;;               (setq path (format "%s/%s-%s" package-dir stem version))
-  ;;               (add-to-list 'load-path path)))
-  ;;           package-alist)))
-
-  (when (fboundp 'run-lac-functions)
-    (run-lac-functions))
+(package-initialize)
+(when (not package-archive-contents)
+  (package-refresh-contents))
 
 
-  (defvar *packages* '())
-  (setq *packages* '(
+;; (defun package-update-load-path ()
+;;   "Update the load path for newly installed packages."
+;;   (interactive)
+;;   (let ((package-dir (expand-file-name package-user-dir)))
+;;     (mapc (lambda (pkg)
+;;             (let ((stem (symbol-name (car pkg)))
+;;                   (version "")
+;;                   (first t)
+;;                   path)
+;;               (mapc (lambda (num)
+;;                       (if first
+;;                         (setq first nil)
+;;                         (setq version (format "%s." version)))
+;;                       (setq version (format "%s%s" version num)))
+;;                     (aref (cdr pkg) 0))
+;;               (setq path (format "%s/%s-%s" package-dir stem version))
+;;               (add-to-list 'load-path path)))
+;;           package-alist)))
 
-                     paredit
+(when (fboundp 'run-lac-functions)
+  (run-lac-functions))
 
-                     emms json popup
 
-                     company auto-complete inf-ruby enh-ruby-mode
-                     smartparens highlight-indentation textmate
-                     flycheck
+(defvar *packages* '())
+(setq *packages* '(
 
-                     ;; intero
-                     stack
-                     ;; ormolu
-                     ;; retrie
-                     ;; shm
-                     ;; ghc
-                     ;; ghc-imported-from
-                     ;; ghci-completion
-                     ;; ac-haskell-process
-                     ;; dante
-                     ;; flycheck-ghcmod
-                     ;; flycheck-haskell
-                     ;; flycheck-hdevtools
-                     ;; flycheck-liquidhs
-                     ;; haskell-emacs
-                     ;; haskell-emacs-base
-                     ;; haskell-emacs-text
-                     ;; haskell-mode
-                     ;; haskell-snippets
-                     ;; haskell-tab-indent
-                     ;; hi2
-                     ;; hindent
-                     ;; hyai
+                   paredit
+
+                   emms json popup
+
+                   company auto-complete inf-ruby enh-ruby-mode
+                   smartparens highlight-indentation textmate
+                   flycheck
+
+                   ;; intero
+                   stack
+                   ;; ormolu
+                   ;; retrie
+                   ;; shm
+                   ;; ghc
+                   ;; ghc-imported-from
+                   ;; ghci-completion
+                   ;; ac-haskell-process
+                   ;; dante
+                   ;; flycheck-ghcmod
+                   ;; flycheck-haskell
+                   ;; flycheck-hdevtools
+                   ;; flycheck-liquidhs
+                   ;; haskell-emacs
+                   ;; haskell-emacs-base
+                   ;; haskell-emacs-text
+                   ;; haskell-mode
+                   ;; haskell-snippets
+                   ;; haskell-tab-indent
+                   ;; hi2
+                   ;; hindent
+                   ;; hyai
                                         ; robe not on emacs-24.4 ?
-                     cobol-mode
+                   cobol-mode
 
-                     lsp-mode
-                     lsp-haskell
+                   lsp-mode
+                   lsp-haskell
+                   lsp-ui
+                   ))
 
-                     ))
+(defun pjb-install-packages ()
+  (interactive)
+  (mapc (lambda (package)
+          (ignore-errors (package-install package)))
+        *packages*))
 
-  (defun pjb-install-packages ()
-    (interactive)
-    (mapc (lambda (package)
-            (ignore-errors (package-install package)))
-          *packages*))
-
-  (pjb-install-packages))
+(pjb-install-packages)
 
 ;; Local Variables:
 ;; coding: utf-8
