03ce96a93520c9d10acf061e64de469c9ea99d8d
Pascal J. Bourguignon <pjb@informatimago.com>
2021-07-09 10:05:55 +0200

    Added emacs-linux.el

---
+++ emacs-linux.el
@@ -0,0 +1,31 @@
+(require 'c-mode)
+
+(defun c-lineup-arglist-tabs-only (ignored)
+  "Line up argument lists by tabs, not spaces"
+  (let* ((anchor (c-langelem-pos c-syntactic-element))
+         (column (c-langelem-2nd-pos c-syntactic-element))
+         (offset (- (1+ column) anchor))
+         (steps (floor offset c-basic-offset)))
+    (* (max steps 1)
+       c-basic-offset)))
+
+(defun linux-c-mode-common-meat ()
+  ;; Add kernel style
+  (c-add-style
+   "linux-tabs-only"
+   '("linux" (c-offsets-alist (arglist-cont-nonempty
+                               c-lineup-gcc-asm-reg
+                               c-lineup-arglist-tabs-only)))))
+
+(add-hook 'c-mode-common-hook 'linux-c-mode-common-meat)
+
+
+(defun linux-c-mode-meat ()
+  (let ((filename (buffer-file-name)))
+    ;; Enable kernel mode for the appropriate files
+    (when (and filename (string-match (expand-file-name "~/works/qorvo/") filename))
+      (setq indent-tabs-mode t)
+      (setq show-trailing-whitespace t)
+      (c-set-style "linux-tabs-only"))))
+
+(add-hook 'c-mode-hook 'linux-c-mode-meat)
