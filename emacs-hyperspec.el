e3107608fd09d02de4f1cca898227603e882fc34
Pascal J. Bourguignon <pjb@informatimago.com>
2022-08-08 10:14:51 +0200

    Updated emacs rc files.

--- emacs-hyperspec.el
+++ emacs-hyperspec.el
@@ -10,6 +10,18 @@
 (require 'clhs      nil t)
 (require 'hyperspec nil t)
 
+(defun pjb-sync-get-resource-at-url (url)
+  (let ((done nil))
+    (url-http (url-generic-parse-url url)
+              (lambda (&rest args)
+                (message "args = %S" args)
+                (message "current buffer = %S" (current-buffer))
+                (setf done (buffer-substring (point-min) (point-max))))
+              nil)
+    (while (not done)
+           (sleep-for 0.1))
+    done))
+
 (defun probe-url (url)
   (cond
     ((string= "file://" (subseq url 0 (min (length url) 7)))
@@ -17,10 +29,9 @@
     ((file-readable-p "/tmp/no-internet")
      nil)
     (t
-     (zerop (first (cl:parse-integer
-                    (shell-command-to-string
-                     (format "wget -O /dev/null %S >/dev/null 2>&1 ; echo -n $?"
-                             url))))))))
+     (handler-case (progn (pjb-sync-get-resource-at-url url)
+                          t)
+       (error () nil)))))
 
 
 (defun pjb-get-resource-at-url (url)
@@ -30,13 +41,7 @@
      (with-file ((subseq url 7) :save nil :kill t :literal t)
        (buffer-substring-no-properties (point-min) (point-max))))
     (t
-     (let ((result (shell-command-to-string
-                    (format "PATH='%s' wget --no-convert-links -q -nv -o /dev/null -t 3  -O -  %s"
-                            (mapconcat (function identity) exec-path ":")
-                            (shell-quote-argument url)))))
-       (if (string= result "/bin/bash: wget: command not found\n")
-           (error result)
-           result)))))
+     (pjb-sync-get-resource-at-url url))))
 
 
 (defparameter *clhs-lispworks* "www.lispworks.com/documentation/HyperSpec/")
