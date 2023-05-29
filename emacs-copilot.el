70190f8c9e3e5d922e86a74892f5f26e140c70f3
Pascal J. Bourguignon <pjb@informatimago.com>
2023-05-29 11:40:01 +0200

    Added chatgpt-shell.

--- emacs-copilot.el
+++ emacs-copilot.el
@@ -1,9 +1,10 @@
-(add-to-list 'load-path "~/emacs/copilot")
-(add-to-list 'load-path "~/emacs/s")
-(add-to-list 'load-path "~/emacs/editorconfig")
+(add-to-list 'load-path "~/emacs/copilot")       ;; git@github.com:zerolfx/copilot.el.git       
+(add-to-list 'load-path "~/emacs/s")             ;; git@github.com:magnars/s.el.git         
+(add-to-list 'load-path "~/emacs/editorconfig")  ;; git@github.com:editorconfig/editorconfig.git
 
 (require 'copilot)
 
+;; sudo port install nodejs16
 ;; (push "~/opt/lib/nodejs/node-v16.16.0-linux-x64/bin" exec-path)
 (add-hook 'prog-mode-hook 'copilot-mode)
 
